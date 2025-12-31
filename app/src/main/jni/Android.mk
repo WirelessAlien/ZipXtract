51687b722a55bc2a723bcc50455f80003a809c1e
WirelessAlien <121420261+WirelessAlien@users.noreply.github.com>
2025-12-31 09:48:53 +0530

    test 2

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -6,62 +6,63 @@ LOCAL_PATH :=$(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := unrar
 LOCAL_CPPFLAGS += -DLITTLE_ENDIAN
-LOCAL_CFLAGS +=  -DANDROID -DRARDLL   -DLITTLE_ENDIAN -DUNIX_TIME_NS\
+LOCAL_CFLAGS +=  -DANDROID -DRARDLL   -DLITTLE_ENDIAN \
   #-D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE
 #-fshort-wchar
-# UnRar lib directory name
-UNRAR_SRC := unrar-6.1.7
+
 LOCAL_CPP_FEATURES := exceptions
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
-LOCAL_SRC_FILES +=${UNRAR_SRC}/archive.cpp \
-                ${UNRAR_SRC}/arcread.cpp \
-                ${UNRAR_SRC}/blake2s.cpp \
-                ${UNRAR_SRC}/cmddata.cpp \
-                ${UNRAR_SRC}/consio.cpp \
-                ${UNRAR_SRC}/crc.cpp \
-                ${UNRAR_SRC}/crypt.cpp \
-                ${UNRAR_SRC}/dll.cpp \
-                ${UNRAR_SRC}/encname.cpp \
-                ${UNRAR_SRC}/errhnd.cpp \
-                ${UNRAR_SRC}/extinfo.cpp \
-                ${UNRAR_SRC}/extract.cpp \
-                ${UNRAR_SRC}/filcreat.cpp \
-                ${UNRAR_SRC}/file.cpp \
-                ${UNRAR_SRC}/filefn.cpp \
-                ${UNRAR_SRC}/filestr.cpp \
-                ${UNRAR_SRC}/find.cpp \
-                ${UNRAR_SRC}/getbits.cpp \
-                ${UNRAR_SRC}/global.cpp \
-                ${UNRAR_SRC}/hash.cpp \
-                ${UNRAR_SRC}/headers.cpp \
-                ${UNRAR_SRC}/list.cpp \
-                ${UNRAR_SRC}/match.cpp \
-                ${UNRAR_SRC}/options.cpp \
-                ${UNRAR_SRC}/pathfn.cpp \
-                ${UNRAR_SRC}/qopen.cpp \
-                ${UNRAR_SRC}/rar.cpp \
-                ${UNRAR_SRC}/rarvm.cpp \
-                ${UNRAR_SRC}/rawread.cpp \
-                ${UNRAR_SRC}/rdwrfn.cpp \
-                ${UNRAR_SRC}/resource.cpp \
-                ${UNRAR_SRC}/rijndael.cpp \
-                ${UNRAR_SRC}/scantree.cpp \
-                ${UNRAR_SRC}/secpassword.cpp \
-                ${UNRAR_SRC}/sha1.cpp \
-                ${UNRAR_SRC}/sha256.cpp \
-                ${UNRAR_SRC}/smallfn.cpp \
-                ${UNRAR_SRC}/strfn.cpp \
-                ${UNRAR_SRC}/strlist.cpp \
-                ${UNRAR_SRC}/system.cpp \
-                ${UNRAR_SRC}/timefn.cpp \
-                ${UNRAR_SRC}/ulinks.cpp \
-                ${UNRAR_SRC}/ui.cpp \
-                ${UNRAR_SRC}/unicode.cpp \
-                ${UNRAR_SRC}/unpack.cpp \
-                ${UNRAR_SRC}/volume.cpp
+LOCAL_SRC_FILES +=unrar-6.1.7/archive.cpp \
+                unrar-6.1.7/arcread.cpp \
+                unrar-6.1.7/blake2s.cpp \
+                unrar-6.1.7/cmddata.cpp \
+                unrar-6.1.7/consio.cpp \
+                unrar-6.1.7/crc.cpp \
+                unrar-6.1.7/crypt.cpp \
+                unrar-6.1.7/dll.cpp \
+                unrar-6.1.7/encname.cpp \
+                unrar-6.1.7/errhnd.cpp \
+                unrar-6.1.7/extinfo.cpp \
+                unrar-6.1.7/extract.cpp \
+                unrar-6.1.7/filcreat.cpp \
+                unrar-6.1.7/file.cpp \
+                unrar-6.1.7/filefn.cpp \
+                unrar-6.1.7/filestr.cpp \
+                unrar-6.1.7/find.cpp \
+                unrar-6.1.7/getbits.cpp \
+                unrar-6.1.7/global.cpp \
+                unrar-6.1.7/hash.cpp \
+                unrar-6.1.7/headers.cpp \
+                unrar-6.1.7/list.cpp \
+                unrar-6.1.7/match.cpp \
+                unrar-6.1.7/options.cpp \
+                unrar-6.1.7/pathfn.cpp \
+                unrar-6.1.7/qopen.cpp \
+                unrar-6.1.7/rar.cpp \
+                unrar-6.1.7/rarvm.cpp \
+                unrar-6.1.7/rawread.cpp \
+                unrar-6.1.7/rdwrfn.cpp \
+                unrar-6.1.7/resource.cpp \
+                unrar-6.1.7/rijndael.cpp \
+                unrar-6.1.7/scantree.cpp \
+                unrar-6.1.7/secpassword.cpp \
+                unrar-6.1.7/sha1.cpp \
+                unrar-6.1.7/sha256.cpp \
+                unrar-6.1.7/smallfn.cpp \
+                unrar-6.1.7/strfn.cpp \
+                unrar-6.1.7/strlist.cpp \
+                unrar-6.1.7/system.cpp \
+                unrar-6.1.7/timefn.cpp \
+                unrar-6.1.7/ulinks.cpp \
+                unrar-6.1.7/ui.cpp \
+                unrar-6.1.7/unicode.cpp \
+                unrar-6.1.7/unpack.cpp \
+                unrar-6.1.7/volume.cpp
 
+LOCAL_LDLIBS := -llog
 
+LOCAL_LDFLAGS := -Wl,--as-needed
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -75,11 +76,6 @@ LOCAL_SRC_FILES = unrardyn.cpp
 LOCAL_LDLIBS := -llog
 
 LOCAL_STATIC_LIBRARIES := unrar
-
-LOCAL_LDLIBS := -llog
-LOCAL_LDFLAGS := -Wl,--as-needed
-
 include $(BUILD_SHARED_LIBRARY)
-
 
 
